3140e0c0bc78fe65acea9885259074927dc0435d
Bohdan <b.liesnikov+git@gmail.com>
2024-03-26 15:54:56 +0100

    maybe we don't need any nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,16 +1,12 @@
 {
   description = "a derivation to build Agda libraries";
 
-  inputs.nixpkgs.url = github:NixOS/nixpkgs;
   inputs.flake-utils.url = github:numtide/flake-utils;
 
-  outputs = {self, nixpkgs, flake-utils}:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {inherit system;};
-      in {
-        lib = {
-          mkAgdaDerivation = pkgs.callPackage ./mkAgdaDerivation.nix {};
-        };
-      });
+  outputs = {self, flake-utils}:
+    {
+      lib = {
+        mkAgdaDerivation = import ./mkAgdaDerivation.nix;
+      };
+    };
 }
